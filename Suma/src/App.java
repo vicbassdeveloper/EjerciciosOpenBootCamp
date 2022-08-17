4b065f39d1639632f320a592f55b5d3077c2e6cf
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-08-17 13:43:36 -0500

    proyecto suma 17 08 2022

---
+++ Suma/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+
+        int a = 10;
+        int b = 10;
+        int c = 10;
+
+        suma(a, b, c);
+
+    }
+
+    public static int suma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+
+}
