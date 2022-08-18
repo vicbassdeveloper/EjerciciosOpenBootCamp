18a979577387a53e1942d958117cbfc8582ec0cc
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-08-18 18:45:31 -0500

    Sentencia While Ejercicios tema 4

---
+++ Introduccion a la Programacion/Ejercicios_tema4/Sentencia While/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+
+    /*
+    
+    * Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+     
+     * Incrementar el valor de la variable en uno cada vez que se ejecute.
+     
+     * Mostrarlo por pantalla cada vez que se ejecute.
+     
+    */
+
+    public static void main(String[] args) throws Exception {
+        int a = -8;
+
+        while ( a < 3)  {
+            System.out.println(a);
+
+            a++;
+        }
+    }
+}
