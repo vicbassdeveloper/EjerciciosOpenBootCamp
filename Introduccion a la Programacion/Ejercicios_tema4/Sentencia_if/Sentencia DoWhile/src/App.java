254b53170cb97e409d5ed9b36d84d2ae3d4c5b31
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-08-18 18:48:29 -0500

    Sentencia DoWhile Ejercicios Tema4

---
+++ Introduccion a la Programacion/Ejercicios_tema4/Sentencia_if/Sentencia DoWhile/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int a = 2;
+
+        do {
+            System.out.println(a);
+
+            a++;
+        }
+        while (a < 3);
+
+    }
+}
