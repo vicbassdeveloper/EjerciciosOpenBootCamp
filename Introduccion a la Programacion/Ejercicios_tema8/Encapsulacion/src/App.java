c874971e88401f797fa19defbe68d5c9195a16c8
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-08-19 14:14:49 -0500

    EjercicioTema8_Encapuslamiento @VicAlex

---
+++ Introduccion a la Programacion/Ejercicios_tema8/Encapsulacion/src/App.java
@@ -0,0 +1,52 @@
+public class App {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+       
+        persona1.setEdad(29);
+        persona1.setNombre("Victor");
+        persona1.setTelefono(55123456);
+    
+    
+        System.out.println(persona1.getEdad());
+        System.out.println(persona1.getName());
+        System.out.println(persona1.getTelefono());
+
+    
+    
+    }
+
+    static class Persona  {
+
+        private int edad;
+        private String nombre;
+        private int telefono;
+       
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getName() {
+            return nombre;
+        }
+
+
+        public void setTelefono(int telefono)   {
+            this.telefono = telefono;
+        }
+
+        public int getTelefono()    {
+            return telefono;
+        }
+    }
+    
+}
