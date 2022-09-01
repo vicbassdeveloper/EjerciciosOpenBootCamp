621bc374b1edf2465aded56cfa4e1925a21b9c0d
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-09-01 16:36:50 -0500

    Ejercicios Tema7

---
+++ Curso Python/Tema7/Ejercicio1.py
@@ -0,0 +1,7 @@
+import Operaciones
+
+
+print("La suma de 4 + 4 es:",Operaciones.suma(4,4))
+print("La resta de 8 - 4 es:", Operaciones.resta(8,4))
+print("La multiplicacion de 5 x 5 es :", Operaciones.multiplicacion(5,5))
+print("La division de 8 / 2 es:", Operaciones.division(8,2))
---
+++ Curso Python/Tema7/Ejercicio2.py
@@ -0,0 +1,16 @@
+import time
+
+hora = time.strftime("%H")
+minutos = time.strftime("%M")
+h = int(hora)
+m = int(minutos)
+
+def h_salir():
+    return 18 - h
+def m_salir():
+    return 60 - m
+
+if h >= 19:
+    print("Es hora de ir a casa")
+else:
+    print("Faltan",h_salir(), "horas con", m_salir(), "minutos para salir del trabajo")
