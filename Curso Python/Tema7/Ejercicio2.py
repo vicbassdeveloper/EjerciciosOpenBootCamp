e235135822646775df176dc6f5e9ce6235a28382
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-09-01 18:43:03 -0500

    MET7

--- Curso Python/Tema7/Ejercicio2.py
+++ Curso Python/Tema7/Ejercicio2.py
@@ -5,12 +5,7 @@ minutos = time.strftime("%M")
 h = int(hora)
 m = int(minutos)
 
-def h_salir():
-    return 18 - h
-def m_salir():
-    return 60 - m
-
 if h >= 19:
     print("Es hora de ir a casa")
 else:
-    print(f"Faltan {h_salir()} horas con {m_salir()} minutos para salir del trabajo")
+    print(f"Faltan {18 - h} horas con {60 - m} minutos para salir del trabajo")
