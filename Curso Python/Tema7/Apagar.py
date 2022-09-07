27045dae91a83eca332d0b7c04c09c08778aade6
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-09-06 19:02:28 -0500

    Ejercicio1 Tema7 @VicAlex

---
+++ Curso Python/Tema7/Apagar.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+h = int(time.strftime("%H"))
+m = int(time.strftime("%M"))
+
+
+if h >= 13:
+    print(f"Ya son las {h}:{m} es hora de ir a casa")
+    os.system("shutdown /s /t 300 /c \"Huye!!! el equipo explotara en 5 minutos, cierra todo y largo de aqui") 
+else:
+    print(f"La hora es {h}:{m}. Faltan {18 - h}:{60 - m} hrs para salir del trabajo")
