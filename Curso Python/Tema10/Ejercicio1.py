1fbcaca9446d95055b70eb3c01f7950a740cf77c
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-09-07 18:55:43 -0500

    Ejercicio1 Tema10 @VicAlex

---
+++ Curso Python/Tema10/Ejercicio1.py
@@ -0,0 +1,31 @@
+# En este ejercicio tenéis que crear una lista de RadioButton que muestre la opción que se ha seleccionado y que contenga un botón de reinicio 
+# para que deje todo como al principio.
+# Al principio no tiene que haber una opción seleccionada.
+
+
+from tkinter import *
+
+def seleccionar():
+    monitor.config(text="{}".format(opcion.get()))
+def reset():
+    opcion.set(None)
+    monitor.config(text="")
+
+root = Tk()
+opcion = StringVar()
+opcion.set(None)
+Radiobutton(root, text="Peugeot", variable=opcion, 
+            value='Peugeot', command=seleccionar).pack(anchor=W)
+
+Radiobutton(root, text="Opcion1", variable=opcion, 
+            value="Opcion1", command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Opcion2", variable=opcion,   
+            value='Opcion2', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Opcion3", variable=opcion,   
+            value='Opcion3', command=seleccionar).pack(anchor=W)
+
+monitor = Label(root)
+monitor.pack()
+Button(root, text="Reiniciar", command=reset).pack()
+
+root.mainloop()
