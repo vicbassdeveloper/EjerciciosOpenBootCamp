a77f6ef063caf3a4f52978175af9e52d73c15796
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2022-11-04 16:05:34 -0600

    Ejercicio1

---
+++ Curso JavaScript/Ejercicio1-primer_objeto.js
@@ -0,0 +1,15 @@
+const yo = {
+    nombre: "Victor",
+    edad: 29,
+    "¿Eres desarrollador?": true,
+    fecha_de_nacimiento: new Date(1993, 7, 30),
+    libro_favorito: {
+        titulo: "El castillo de Otranto",
+        autor: "Horacio Walpole",
+        fecha: new Date(1764),
+        url: "https://biblioteca.org.ar/libros/154027.pdf"
+    }
+
+
+}
+
