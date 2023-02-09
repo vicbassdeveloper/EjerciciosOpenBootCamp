8019bf4959dc5e19fb05e208061d3183e10c7bd4
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2023-02-09 16:49:10 -0600

    Ejercicio3 Bucles

---
+++ Java/ob-ejercicio1/src/com/ejercicio/Bucles.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.Arrays;
+
+/**
+ * Crear un bucle que permita concatenar texto e imprima el resultado final por consola
+ * Concatenar nombres
+ * Los textos pueden venir de un array String
+ * String[] nombres = {"", "", "", ""};
+ */
+public class Bucles {
+
+    public static void main(String[] args) {
+        String[] listaNombres = {"Maria", "Juan", "Fransisco", "Ana", "Margarita"};
+
+        System.out.println(nombres(listaNombres));
+
+
+    }
+
+    public static String nombres(String[] nombres) {
+        String nombre = " ";
+
+        for (int i = 0; i < nombres.length; i++) {
+
+            nombre += nombres[i] + " ";
+        }
+        return nombre;
+    }
+}
