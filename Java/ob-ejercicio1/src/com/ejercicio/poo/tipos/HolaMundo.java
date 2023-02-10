3dc66141d47bae7c44196d35e464071fa2fc5ae1
vicbassdeveloper <44815768+vicbassdeveloper@users.noreply.github.com>
2023-02-10 13:24:58 -0600

    Delete  Java Directory

--- Java/ob-ejercicio1/src/com/ejercicio/poo/tipos/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-package com.example.poo.tipos;
-
-public class HolaMundo {
-
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo desde sesion 1 de OpenBootcamp");
-    }
-
-}
