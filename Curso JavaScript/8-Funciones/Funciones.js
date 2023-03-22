0fe5305090a2cbc5c4aaa1f0af2c31456a91a784
vicbassdeveloper <victor16alex@gmail.com>
2023-03-22 12:31:37 -0600

    Ejercicio8 JS

---
+++ Curso JavaScript/8-Funciones/Funciones.js
@@ -0,0 +1,35 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una función sin parámetros que devuelva siempre "true"
+
+function devuelveVerdadero() {
+    return true
+}
+
+console.log(devuelveVerdadero())
+
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+
+function promesa () {
+    setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+promesa();
+
+
+// - Una función generadora de índices pares automáticos
+
+function pares() {
+    let id = 0;
+
+    while(true){
+        id += 2;
+    }
+
+}
+
+console.log(pares());
+
+
+
+
